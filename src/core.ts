74a629ab67d238acc9f66b8879ca6ee1a5965230
tjmtmmnk <tsujimoto027@gmail.com>
2020-03-07 23:57:48 +0900

    coreに移す

---
+++ src/core.ts
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+
+import { Scanner } from './scanner';
+import { DB } from './db';
+
+export class Core {
+    private workspace: vscode.WorkspaceFolder | undefined;
+
+    constructor(private context: vscode.ExtensionContext) {
+        this.workspace = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0] : undefined;
+    }
+
+    public attatchCommands(): void {
+        const scanCommand = vscode.commands.registerCommand('extension.scanFiles', () => {
+            vscode.window.showInformationMessage('Hello World!');
+            const scanner = new Scanner(vscode.workspace.getConfiguration('autouse'));
+            scanner.scan(this.workspace);
+        });
+
+        const showDBCommand = vscode.commands.registerCommand('extension.showDB', () => {
+            console.log(DB.all());
+        });
+
+        const selectUseCommand = vscode.commands.registerCommand('extension.selectUse', () => {
+
+        });
+
+        this.context.subscriptions.push(scanCommand, showDBCommand, selectUseCommand);
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { Scanner } from './scanner';
-import { DB } from './db';
+import { Core } from './core';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,24 +11,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "auto-use" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const core = new Core(context);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-		const scanner = new Scanner(vscode.workspace.getConfiguration('autouse'));
-		const workspace = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0] : undefined;
-		scanner.scan(workspace);
-	});
-
-	const disposable2 = vscode.commands.registerCommand('extension.showDB', () => {
-		console.log(DB.all());
-	});
-
-	context.subscriptions.push(disposable, disposable2);
+	core.attatchCommands();
 }
 
 // this method is called when your extension is deactivated
