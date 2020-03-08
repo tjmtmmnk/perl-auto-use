c47539eaa6323b6e0c339b840a75ec5de0ba0234
tjmtmmnk <tsujimoto027@gmail.com>
2020-03-08 23:12:15 +0900

    position, rangeを正規表現で取得する関数を追加

--- src/selector.ts
+++ src/selector.ts
@@ -6,6 +6,56 @@ interface UseSubObject {
 }
 
 export class Selector {
+    private getFirstLineByRegex(regex: RegExp): number | undefined {
+        const editor = vscode.window.activeTextEditor;
+        const document = editor?.document;
+        if (editor === undefined || document === undefined) { return undefined; }
+
+        for (let i = 0; i < document.lineCount; i++) {
+            const line = document?.lineAt(i).text;
+            const packageMatches = line.match(regex);
+            if (packageMatches) { return i + 1; }
+        }
+    }
+
+    private getStartPositionsByRegex(regex: RegExp): vscode.Position[] | undefined {
+        const editor = vscode.window.activeTextEditor;
+        const document = editor?.document;
+        const fullText = document?.getText();
+
+        if (editor === undefined || fullText === undefined) { return undefined; }
+
+        const matches = fullText.matchAll(regex);
+        let startPositions = new Array<vscode.Position>();
+        for (const match of matches) {
+            const startIndex = match.index ? match.index : 0;
+            const startPosition = editor.document.positionAt(startIndex);
+            startPositions.push(startPosition);
+        }
+
+        return startPositions;
+    }
+
+    private getRangesByRegex(regex: RegExp): vscode.Range[] | undefined {
+        const editor = vscode.window.activeTextEditor;
+        const document = editor?.document;
+        const fullText = document?.getText();
+
+        if (editor === undefined || fullText === undefined) { return undefined; }
+
+        const matches = fullText.matchAll(regex);
+        let ranges = new Array<vscode.Range>();
+        for (const match of matches) {
+            const startIndex = match.index ? match.index : 0;
+            const endIndex = match.index ? match.index + match[0].length : 0;
+            const startPosition = editor.document.positionAt(startIndex);
+            const endPosition = editor.document.positionAt(endIndex);
+            ranges.push(new vscode.Range(startPosition, endPosition));
+        }
+
+        return ranges;
+    }
+
     public getSelectText(): string {
         const editor = vscode.window.activeTextEditor;
         const document = editor?.document;
@@ -16,19 +66,9 @@ export class Selector {
     // package の次の行にuseを挿入する
     public insertUseSelection(useStatement: string): void {
         const editor = vscode.window.activeTextEditor;
-        const document = editor?.document;
+        const startLine = this.getFirstLineByRegex(new RegExp(/package [A-Za-z0-9:]+;/, 'g'));
 
-        if (document === undefined) { return; }
-
-        let startLine = 0;
-        for (let i = 0; i < document.lineCount; i++) {
-            const line = document?.lineAt(i).text;
-            const packageMatches = line.match(/package [A-Za-z0-9:]+;/g);
-            if (packageMatches) {
-                startLine = i + 1;
-                break;
-            }
-        }
+        if (startLine === undefined) { return; }
 
         const position = new vscode.Position(startLine, 0);
         editor?.edit(e => e.insert(position, useStatement + "\n"));
@@ -56,7 +96,7 @@ export class Selector {
         const document = editor?.document;
         const fullText = document?.getText();
         const useSubMatches = fullText?.match(/use [A-Za-z0-9:]+ qw(\/|\()(\s*\w+\s*)*(\/|\));/g);
-    
+
         return useSubMatches?.map(u => {
             const packageName = u
                 .replace('use ', '')
