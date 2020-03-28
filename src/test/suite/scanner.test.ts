a1308009461f94dcf2eed382bd7b6f9ac04fa79a
tjmtmmnk <tsujimoto027@gmail.com>
2020-03-29 00:04:20 +0900

    scannerのテスト

---
+++ src/test/suite/scanner.test.ts
@@ -0,0 +1,26 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+import { DB, ImportObject } from '../../db';
+
+import { MockAutoUseContext } from '../src/mockAutoUseContext';
+import { Scanner } from '../../scanner';
+import { join } from 'path';
+
+
+suite('Extension Test Suite', () => {
+    setup(() => {
+        DB.deleteAll();
+    });
+
+    test('rootPath', () => {
+        assert.strictEqual(vscode.workspace.workspaceFolders!.length, 1);
+        assert.strictEqual(vscode.workspace.rootPath!, join(__dirname, '../../../src/test/testWorkSpace'));
+    });
+
+    test('scan', async () => {
+        const scanner = new Scanner(MockAutoUseContext);
+        await scanner.scan();
+        assert.strictEqual(DB.all().length, 6, 'get six');
+    });
+});
