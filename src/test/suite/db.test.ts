aa6f4f3aafc9561681ab38907a6ba5e24e83e5d0
tjmtmmnk <tsujimoto027@gmail.com>
2020-03-29 17:00:16 +0900

    scannner内でコンテキストをモック

--- src/test/suite/db.test.ts
+++ src/test/suite/db.test.ts
@@ -1,11 +1,9 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
+
 import { DB, ImportObject } from '../../db';
 
-suite('Extension Test Suite', () => {
+suite('DB Test', () => {
 	setup(() => {
 		DB.deleteAll();
 	});
